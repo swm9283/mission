23a6736648b490556b368a61c4da1a37fb5d0332
uminsong1512 <uminsong1512@gmail.com>
2023-01-16 17:07:23 +0900

    5.6수정,5.7

--- day03_mission.py
+++ day03_mission.py
@@ -1,5 +1,12 @@
-#mission1
+#mission
 #5.6
+list = ["duck","gourd","spitz"]
+for i in list:
+    k=i.title()
+    print("%sy Mc%sface" % (k,k))
+
+
+#5.7
 list = ["duck","gourd","spitz"]
 for i in list:
     print("{k}y Mc{k}face".format(k=i.title()))
