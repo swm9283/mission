45102549ef08a85a032e5f2c69a04bf6d825accf
uminsong1512 <uminsong1512@gmail.com>
2023-01-16 09:30:45 +0900

    mission 2 보충

--- day02_mission.py
+++ day02_mission.py
@@ -3,8 +3,13 @@
 # 완두콩은 작고 녹색이다
 # 수박은 크고 녹색이다
 # 호박은 크고 녹색이 아니다
-small = True
-green = True
+# small = True
+# green = True
+import random
+small = random.choice([True,False]) #리스트에서 랜덤하게 선택한다
+green = random.choice([True,False]) #리스트에서 랜덤하게 선택한다
+
+
 if small:
     if green:
         print("pea")
